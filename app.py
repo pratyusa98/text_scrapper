df11ee448e3d7e185a3ff1395a0cf9389ef75749
Pratyusa dwibedy <40734280+pratyusa98@users.noreply.github.com>
2020-09-20 15:10:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+# doing necessary imports
+import os
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+#import pymongo
+
+app = Flask(__name__)  # initialising the flask app with the name 'app'
+
+
+
+
+@app.route('/',methods=['POST','GET']) # route with allowed methods as POST and GET
+@cross_origin()
+def index():
+
+    if request.method == 'POST':
+
+        try:
+
+            searchString = request.form['content'].replace(" ","") 
+            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
+            uClient = uReq(flipkart_url) 
+            flipkartPage = uClient.read() 
+            uClient.close() 
+            flipkart_html = bs(flipkartPage, "html.parser") 
+            bigboxes = flipkart_html.findAll("div", {"class": "bhgxx2 col-12-12"})
+            del bigboxes[0:3] 
+            box = bigboxes[0] 
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href'] 
+            prodRes = requests.get(productLink) 
+            prod_html = bs(prodRes.text, "html.parser") 
+            commentboxes = prod_html.find_all('div', {'class': "_3nrCtb"}) 
+
+            filename = searchString+".csv"
+            fw = open(filename, "w")
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            fw.write(headers)
+            reviews = []
+            for commentbox in commentboxes:
+
+                try:
+                    name = commentbox.div.div.find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text
+                except:
+                    name = 'No Name'
+                try:
+                    rating = commentbox.div.div.div.div.text
+                except:
+                    rating = 'No Rating'
+
+                try:
+                    commentHead = commentbox.div.div.div.p.text
+                except:
+                    commentHead = 'No Comment Heading'
+                try:
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    custComment = comtag[0].div.text
+                except:
+                    custComment = 'No Customer Comment'
+
+                mydict = {"Product": searchString, "Name": name, "Rating": 
+                       rating, "CommentHead": commentHead, "Comment": custComment}
+
+                reviews.append(mydict)
+            return render_template('index.html', reviews=reviews)    
+        except:
+            return 'something is wrong'
+            #return render_template('results.html')
+    else:
+        return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True) # running the app on the local machine on port 8000
